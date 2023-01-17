9eed62446fcc7eb5987252a0865c5c888d586680
Pierre Sarnow <psarnow@gmail.com>
2023-01-17 14:02:39 -0700

    Update streamlit_app.py
    new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🍝 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🫑, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🥪 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
